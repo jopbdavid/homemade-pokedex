642f1a49ed43f899a578056daaf8b110df44cbce
jopbdavid <jopbdavid@gmail.com>
2023-05-10 16:20:20 +0100

    first commit deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import pokedex from "./img/pokedex.png";
+import logo from "./img/Pokédex_logo.png";
+import axios from "axios";
+
+const url = "https://pokeapi.co/api/v2/pokemon/";
 
 function App() {
+  const [pokemon, setPokemon] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [index, setIndex] = useState(1);
+  const [query, setQuery] = useState("1");
+
+  const getPokemon = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(`${url}${index}`);
+      const { id, name } = response.data;
+      const img =
+        response.data.sprites.versions["generation-viii"].icons.front_default;
+
+      console.log(response);
+      setPokemon({ id: id, name: name, image: img });
+      setIndex(id);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  const handleNext = () => {
+    setIndex(index + 1);
+
+    console.log(index);
+  };
+  const handlePrev = () => {
+    setIndex(index - 1);
+
+    console.log(index);
+  };
+
+  useEffect(() => {
+    getPokemon();
+  }, [index]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img src={logo} alt="logo" className="title" />
+      <main className="pokedex-body">
+        <img src={pokedex} className="pokedex-img" />
+        {!loading ? (
+          <img src={pokemon.image} alt={pokemon.name} className="pokemon-img" />
+        ) : (
+          <img src="" className="pokemon-img" />
+        )}
+        <div className="pokemon-name">
+          {!loading ? <h1>{pokemon.name}</h1> : <h1>Loading...</h1>}
+        </div>
+        <button type="button" className="btn-prev" onClick={handlePrev}>
+          Prev
+        </button>
+        <button type="button" className="btn-next" onClick={handleNext}>
+          Next
+        </button>
+      </main>
+    </>
   );
 }
 
